688bd23efc3e11ae8951360864351ee85ada30c1
Ryan <ryanaouiche@gmail.com>
2024-03-13 14:21:16 +0100

    try to fix ef core database update

--- bibliotheque/Models/ApiContext.cs
+++ bibliotheque/Models/ApiContext.cs
@@ -11,6 +11,6 @@ public class ApiContext : DbContext
 
     public DbSet<Auteur> Auteurs { get; set; } = null!;
     public DbSet<Client> Clients { get; set; } = null!;
-    public DbSet<Medias> Medias { get; set; } = null!;
+    public DbSet<Media> Medias { get; set; } = null!;
     public DbSet<Reservation> Reservations { get; set; } = null!;
 }
