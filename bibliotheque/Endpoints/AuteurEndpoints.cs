5491216ce5d7c673b7905000dc0ecf82fc136663
ryan-1337 <ryanaouiche@gmail.com>
2024-03-22 15:44:26 +0100

    refacto AuteurEndpoint

--- bibliotheque/Endpoints/AuteurEndpoints.cs
+++ bibliotheque/Endpoints/AuteurEndpoints.cs
@@ -1,5 +1,7 @@
 using bibliotheque.Models;
+using Microsoft.AspNetCore.Http.HttpResults;
 using Microsoft.EntityFrameworkCore;
+using static Microsoft.AspNetCore.Http.TypedResults;
 
 namespace bibliotheque.Endpoints;
 
@@ -7,67 +9,80 @@ public static class AuteurEndpoints
 {
     public static void MapAuteur(this IEndpointRouteBuilder app)
     {
-        app.MapGet("/api/Auteurs", async (ApiContext context) => await context.Auteurs.ToListAsync())
+        app.MapGet("/api/Auteurs", GetAllAuteur)
+            .Produces(StatusCodes.Status200OK)
             .WithTags("Auteurs");
 
-        app.MapGet("/api/Auteurs/{id}", async (ApiContext context, int id) =>
-            {
-                var auteur = await context.Auteurs.FindAsync(id);
-                if (auteur == null)
-                {
-                    return Results.NotFound();
-                }
-
-                return Results.Ok(auteur);
-            })
+        app.MapGet("/api/Auteurs/{id}", GetAuteurById)
             .WithTags("Auteurs");
 
-        app.MapPut("/api/Auteurs/{id}", async (ApiContext context, int id, Auteur auteur) =>
-            {
-                if (id != auteur.Id)
-                {
-                    return Results.BadRequest();
-                }
-
-                context.Entry(auteur).State = EntityState.Modified;
-
-                if (AuteurExists(context, id))
-                {
-                    await context.SaveChangesAsync();
-                    return Results.NoContent();
-                }
-
-                return Results.NotFound();
-            })
+        app.MapPut("/api/Auteurs/{id}", PutAuteur)
             .WithTags("Auteurs");
 
-        app.MapPost("/api/Auteurs", async (ApiContext context, AuteurRequest auteur) =>
-            {
-                var auteurToAdd = new Auteur { FirstName = auteur.FirstName, LastName = auteur.LastName };
-                await context.AddAsync(auteurToAdd);
-                await context.SaveChangesAsync();
-                return Results.NoContent();
-            })
+        app.MapPost("/api/Auteurs", CreateAuteur)
             .WithTags("Auteurs");
 
-        app.MapDelete("/api/Auteurs/{id}", async (ApiContext context, int id) =>
-            {
-                var auteur = await context.Auteurs.FindAsync(id);
-                if (auteur == null)
-                {
-                    return Results.NotFound();
-                }
-
-                context.Auteurs.Remove(auteur);
-                await context.SaveChangesAsync();
-
-                return Results.Ok();
-            })
+        app.MapDelete("/api/Auteurs/{id}", DeleteAuteur)
             .WithTags("Auteurs");
+    }
 
-        bool AuteurExists(ApiContext context, int id)
+    private static async Task<IResult> GetAllAuteur(ApiContext context)
+    {
+        return Ok(await context.Auteurs.ToListAsync());
+    }
+
+    private static async Task<IResult> GetAuteurById(ApiContext context, int id)
+    {
+        return await context.Auteurs.FindAsync(id) 
+            is Auteur auteur
+            ? Ok(auteur)
+            : NotFound();
+    }
+
+    private static async Task<IResult> PutAuteur(ApiContext context, int id, AuteurRequest request)
+    {
+        var auteur = await context.Auteurs.FindAsync(id); 
+        
+        if (auteur == null)
         {
-            return context.Auteurs.Any(e => e.Id == id);
+            return Results.BadRequest();
         }
-    } 
-}
+
+        context.Entry(auteur).State = EntityState.Modified;
+
+        if (AuteurExists(context, id))
+        {
+            await context.SaveChangesAsync();
+            return Results.NoContent();
+        }
+
+        return Results.NotFound();
+    }
+
+    private static async Task<IResult> CreateAuteur(ApiContext context, AuteurRequest auteur)
+    {
+        var auteurToAdd = new Auteur { FirstName = auteur.FirstName, LastName = auteur.LastName };
+        await context.AddAsync(auteurToAdd);
+        await context.SaveChangesAsync();
+        return Results.NoContent();
+    }
+
+    private static async Task<IResult> DeleteAuteur(ApiContext context, int id)
+    {
+        var auteur = await context.Auteurs.FindAsync(id);
+        if (auteur == null)
+        {
+            return Results.NotFound();
+        }
+
+        context.Auteurs.Remove(auteur);
+        await context.SaveChangesAsync();
+
+        return Results.Ok();
+    }
+    
+    private static bool AuteurExists(ApiContext context, int id)
+    {
+        return context.Auteurs.Any(e => e.Id == id);
+    }
+} 
